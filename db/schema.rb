3e3a21ef26c26a6e00045be75b25c69a27e46bda
Oguzhan Yildiz <oxy@OxY-2.local>
2019-04-25 18:25:10 +0200

    Create model for courses along with database connection

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_19_174848) do
+ActiveRecord::Schema.define(version: 2019_04_25_155230) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,18 @@ ActiveRecord::Schema.define(version: 2019_04_19_174848) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
+
+  create_table "courses", force: :cascade do |t|
+    t.string "code"
+    t.string "name"
+    t.string "instructor"
+    t.time "period1"
+    t.time "period2"
+    t.string "venue"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["code"], name: "index_courses_on_code", unique: true
   end
 
   create_table "professors", force: :cascade do |t|
